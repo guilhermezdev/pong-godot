e1f24a5348e7844680973398e99f8ea62ce6fd94
Guilherme Zamberlam Pomini <guizamberlam@hotmail.com>
2024-09-02 17:51:41 -0300

    scoring points

---
+++ Scripts/Ball.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+var win_size : Vector2
+const START_SPEED : int = 500
+const ACCEL : int = 50
+var speed : int
+var dir : Vector2
+
+func _ready():
+	win_size = get_viewport_rect().size
+	
+func _physics_process(delta):
+	var collision = move_and_collide(dir * speed * delta)
+	if collision:
+		var collider = collision.get_collider()
+		if collider == $'../Player' or collider == $'../Player2':
+			speed += ACCEL
+		dir = dir.bounce(collision.get_normal())
+	
+func start_ball():
+	position.x = win_size.x / 2
+	position.y = randi_range(50, win_size.y - 50)
+	speed = START_SPEED
+	dir = random_direction()
+	
+func random_direction():
+	var new_dir = Vector2()
+	new_dir.x = [-1, 1].pick_random()
+	new_dir.y = randf_range(-1, 1)
+	return new_dir.normalized()
---
+++ Scripts/pong.gd
@@ -0,0 +1,22 @@
+extends Node
+
+var score := [0, 0]
+const PADDLE_SPEED : int = 500
+
+func _process(delta):
+	if Input.is_key_pressed(KEY_ESCAPE):
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+func _on_game_timer_timeout():
+	$Ball.start_ball()
+
+func _on_goal_left_body_entered(body):
+	score[1] += 1
+	$Background/Scores/Player2Score.text = str(score[1])
+	$GameTimer.start()
+
+func _on_goal_right_body_entered(body):
+	score[0] += 1
+	$Background/Scores/PlayerScore.text = str(score[0])
+	$GameTimer.start()
