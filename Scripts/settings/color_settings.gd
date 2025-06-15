659e01fc369a7c7ab0a6cd2e1ad983573fd617f9
Guilherme Zamberlam Pomini <guizamberlam@hotmail.com>
2025-06-14 22:01:15 -0300

    updates

--- Scripts/settings/color_settings.gd
+++ Scripts/settings/color_settings.gd
@@ -4,13 +4,11 @@ extends Control
 @onready var player_2_color_button = $VBoxContainer/Player2Color/Player2ColorButton
 @onready var field_color_button = $VBoxContainer/FieldColor/FieldColorButton
 @onready var field_divider_color_button = $VBoxContainer/FieldDividerColor/FieldDividerColorButton
-@onready var ball_color_button = $VBoxContainer/BallColor/BallColorButton
 
 var player_1_selected_color: Color
 var player_2_selected_color: Color
 var field_selected_color: Color
 var field_divider_selected_color: Color
-var ball_selected_color: Color
 
 func _ready():
 	var game_settings = ConfigFileHandler.load_game_settings()
@@ -26,16 +24,12 @@ func _ready():
 	
 	field_divider_selected_color = Color(game_settings['field_divider_color'])
 	field_divider_color_button.color = field_divider_selected_color
-	
-	ball_selected_color = Color(game_settings['ball_color'])
-	ball_color_button.color = ball_selected_color
 
 func _on_save_button_pressed():
 	ConfigFileHandler.save_game_setting('player_1_color', player_1_selected_color)
 	ConfigFileHandler.save_game_setting('player_2_color', player_2_selected_color)
 	ConfigFileHandler.save_game_setting('field_color', field_selected_color)
 	ConfigFileHandler.save_game_setting('field_divider_color', field_divider_selected_color)
-	ConfigFileHandler.save_game_setting('ball_color', ball_selected_color)
 
 func _on_player_1_color_button_color_changed(color):
 	player_1_selected_color = color
@@ -48,10 +42,6 @@ func _on_field_color_button_color_changed(color):
 
 func _on_field_divider_color_button_color_changed(color):
 	field_divider_selected_color = color
-
-func _on_ball_color_button_color_changed(color):
-	ball_selected_color = color
-
 
 func _on_back_button_pressed():
 	get_tree().change_scene_to_file("res://scenes/settings/settings.tscn")
